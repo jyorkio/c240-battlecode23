255a8da6b5b974525ecb64afe534f5b28dec363e
msuad <asteward@monmouthcollege.edu>
2023-02-01 20:43:51 -0600

    add carrier and launcher

--- src/elicompbot/RobotPlayer.java
+++ src/elicompbot/RobotPlayer.java
@@ -201,3 +201,89 @@ public class Pathing {
         }
     }
 }
+    static void runCarrier(RobotController rc) throws GameActionException {
+        if (rc.getAnchor() != null) {
+            // If I have an anchor singularly focus on getting it to the first island I see
+            int[] islands = rc.senseNearbyIslands();
+            Set<MapLocation> islandLocs = new HashSet<>();
+            for (int id : islands) {
+                MapLocation[] thisIslandLocs = rc.senseNearbyIslandLocations(id);
+                islandLocs.addAll(Arrays.asList(thisIslandLocs));
+            }
+            if (islandLocs.size() > 0) {
+                MapLocation islandLocation = islandLocs.iterator().next();
+                rc.setIndicatorString("Moving my anchor towards " + islandLocation);
+                while (!rc.getLocation().equals(islandLocation)) {
+                    Direction dir = rc.getLocation().directionTo(islandLocation);
+                    if (rc.canMove(dir)) {
+                        rc.move(dir);
+                    }
+                }
+                if (rc.canPlaceAnchor()) {
+                    rc.setIndicatorString("Huzzah, placed anchor!");
+                    rc.placeAnchor();
+                }
+            }
+        }
+        // Try to gather from squares around us.
+        MapLocation me = rc.getLocation();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation wellLocation = new MapLocation(me.x + dx, me.y + dy);
+                if (rc.canCollectResource(wellLocation, -1)) {
+                    if (rng.nextBoolean()) {
+                        rc.collectResource(wellLocation, -1);
+                        rc.setIndicatorString("Collecting, now have, AD:" +
+                                rc.getResourceAmount(ResourceType.ADAMANTIUM) +
+                                " MN: " + rc.getResourceAmount(ResourceType.MANA) +
+                                " EX: " + rc.getResourceAmount(ResourceType.ELIXIR));
+                    }
+                }
+            }
+        }
+        // Occasionally try out the carriers attack
+        if (rng.nextInt(20) == 1) {
+            RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+            if (enemyRobots.length > 0) {
+                if (rc.canAttack(enemyRobots[0].location)) {
+                    rc.attack(enemyRobots[0].location);
+                }
+            }
+        }
+
+        // If we can see a well, move towards it
+        WellInfo[] wells = rc.senseNearbyWells();
+        if (wells.length > 1 && rng.nextInt(3) == 1) {
+            WellInfo well_one = wells[1];
+            Direction dir = me.directionTo(well_one.getMapLocation());
+            if (rc.canMove(dir))
+                rc.move(dir);
+        }
+        // Also try to move randomly.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+    }
+    static void runLauncher(RobotController rc) throws GameActionException {
+        // Try to attack someone
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        if (enemies.length >= 0) {
+            // MapLocation toAttack = enemies[0].location;
+            MapLocation toAttack = rc.getLocation().add(Direction.EAST);
+
+            if (rc.canAttack(toAttack)) {
+                rc.setIndicatorString("Attacking");
+                rc.attack(toAttack);
+            }
+        }
+
+        // Also try to move randomly.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+    }
+}
